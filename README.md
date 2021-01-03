6f3fe7f67e487dd87373c509f489b9fe4fa1244b
saman2052 <tire_ashkani@yahoo.com>
2021-01-02 22:25:02 -0500

    Uploading my homework 19

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# homework.19
+
+This is an Employee Directory using React and a user can view a list of employee's information such as picture, phone number, email and DOB.
+
+Here is the Demo:
+https://tak-9.github.io/react-employee-directory/
+
+
+Usage:
+* You use the search box to narrow your results.
+* Click on an arrow to sort by heading.
+
+
+
+
+Technologies: 
+* HTML, Bootstrap, JavaScript, React, Axios
+
---
+++ client/build/precache-manifest.acb4502f35b42a288764dcd3c2205e93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b72d0b7647839f931c0e29f1190f7f78",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "dd0e56c7eea48d9e1e4b",
+    "url": "/react-employee-directory/static/js/2.6661f716.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-employee-directory/static/js/2.6661f716.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edeb88f2de98f5f9b072",
+    "url": "/react-employee-directory/static/js/main.63001b9e.chunk.js"
+  },
+  {
+    "revision": "2c41a4f351a0e22603f9",
+    "url": "/react-employee-directory/static/js/runtime-main.afdc8d91.js"
+  }
+]);
